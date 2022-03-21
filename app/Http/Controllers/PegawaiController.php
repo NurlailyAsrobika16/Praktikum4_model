e52327d5442de060586e971d358c1ec2b1bbf4df
asrobika01 <asrobika01@gmail.com>
2022-03-21 08:11:29 +0700

    menambah perintah pada controller

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PegawaiController extends Controller
 {
-    //
+    public function index()
+    {
+    	// mengambil data pegawai
+    	$pegawai = Pegawai::all();
+ 
+    	// mengirim data pegawai ke view pegawai
+    	return view('pegawai', ['pegawai' => $pegawai]);
+    }
+ 
 }
